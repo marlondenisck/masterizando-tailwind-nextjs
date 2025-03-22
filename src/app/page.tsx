8d94d3dc13325a4feeb7b858678bbc3c131d3440
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-22 17:03:06 -0400

    Criando Projeto com Tailwind e nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-4 bg-gray-100 h-screen">
+      <h1 className="text-teste">Ola mundo</h1>
+      <h2 className="font-bold text-5xl">Ola tailwind</h2>
+    </div>
+  );
+}
