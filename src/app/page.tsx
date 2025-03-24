ef1062bcfcc9861627d219dac6f3eee04ab0d318
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-24 15:17:25 -0400

    Dark Mode no Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <div className="p-4 bg-slate-900 text-slate-50 h-screen">
-      <h1 className="flex items-center gap-2 before:w-1 before:h-4 before:bg-sky-50">Ola mundo</h1>
-      <h2 className="font-bold text-3xl sm:text-5xl lg:text-6xl">Ola tailwind</h2>
-      <button disabled className="bg-sky-500 px-4 py-2 rounded mt-4 cursor-pointer enabled:hover:bg-sky-600 disabled:opacity-60  disabled:cursor-not-allowed">Entrar</button>
+    <div className="dark:bg-slate-900 dark:text-slate-50 p-4 bg-slate-0 text-slate-900 h-screen flex flex-col items-center justify-center text-center">
+      <div className="max-w-2xl mx-auto">
+        <h1 className="flex items-center gap-2 before:w-1 before:h-4 before:bg-sky-50">Ola mundo</h1>
+        <h2 className="font-bold text-3xl md:text-5xl">Ola tailwind</h2>
+        <button disabled className="bg-sky-500 text-white px-4 py-2 rounded mt-4 cursor-pointer enabled:hover:bg-sky-600 disabled:opacity-60  disabled:cursor-not-allowed">Entrar</button>
+      </div>
     </div>
   );
 }
