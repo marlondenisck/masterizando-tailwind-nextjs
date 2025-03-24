cdd26ed93a61a9d9c8eb511f8e692a753c6261fa
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-24 15:05:18 -0400

    Seletores e Estados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <div className="p-4 bg-gray-100 h-screen">
-      <h1 className="text-teste">Ola mundo</h1>
+    <div className="p-4 bg-slate-900 text-slate-50 h-screen">
+      <h1 className="flex items-center gap-2 before:w-1 before:h-4 before:bg-sky-50">Ola mundo</h1>
       <h2 className="font-bold text-5xl">Ola tailwind</h2>
+      <button disabled className="bg-sky-500 px-4 py-2 rounded mt-4 cursor-pointer enabled:hover:bg-sky-600 disabled:opacity-60  disabled:cursor-not-allowed">Entrar</button>
     </div>
   );
 }
