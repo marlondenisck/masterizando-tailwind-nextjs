cadca95c11c2d665f14f7245970fbb4539be11d7
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-03-24 16:15:21 -0400

    Configurando ESLint e Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,22 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Masterizando o Tailwind",
-  description: "Aprendendo tawilind",
-};
+  title: 'Masterizando o Tailwind',
+  description: 'Aprendendo tawilind',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-       className={inter.className}>
-        {children}
-      </body>
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 export default function Home() {
   return (
-    <div className="dark:bg-slate-900 dark:text-slate-50 p-4 bg-slate-0 text-slate-900 h-screen flex flex-col items-center justify-center text-center">
-      <div className="max-w-[700px] mx-auto">
-        <h1 className="flex items-center gap-2 before:w-1 before:h-4 before:bg-sky-50">Ola mundo</h1>
-        <h2 className="font-bold text-3xl md:text-5xl">Ola tailwind</h2>
-        <button disabled className="bg-sky-500 text-white px-4 py-2 rounded mt-4 cursor-pointer enabled:hover:bg-sky-600 disabled:opacity-60  disabled:cursor-not-allowed">Entrar</button>
+    <div className='bg-slate-0 flex h-screen flex-col items-center justify-center p-4 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-50'>
+      <div className='mx-auto max-w-[700px]'>
+        <h1 className='flex items-center gap-2 before:h-4 before:w-1 before:bg-sky-50'>
+          Ola mundo
+        </h1>
+        <h2 className='text-3xl font-bold md:text-5xl'>Ola tailwind</h2>
+        <button
+          disabled
+          className='mt-4 cursor-pointer rounded bg-sky-500 px-4 py-2 text-white enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60'
+        >
+          Entrar
+        </button>
       </div>
     </div>
-  );
+  )
 }
