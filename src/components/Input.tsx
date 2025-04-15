c418fe9d43c9941d6e7f9f724acc6b5cc41e9d81
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-04-14 20:58:02 -0400

    Pattern de Composição

---
+++ src/components/Input.tsx
@@ -0,0 +1,36 @@
+import { ComponentProps } from 'react'
+
+type InputPrefixProps = ComponentProps<'div'>
+
+function InputIcon(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+type InputControlProps = ComponentProps<'input'>
+
+function InputControl(props: InputControlProps) {
+  return (
+    <input
+      type='text'
+      className='flex-1 border-0 bg-transparent p-0 text-sm text-zinc-900 placeholder:text-zinc-600 focus:outline-none'
+      {...props}
+    />
+  )
+}
+
+type InputRootProps = ComponentProps<'div'>
+
+function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      className='flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm'
+      {...props}
+    />
+  )
+}
+
+export const Input = {
+  Root: InputRoot,
+  Icon: InputIcon,
+  Control: InputControl,
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,20 +13,19 @@ import {
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import { Input } from '@/components/Input'
 
 export function Sidebar() {
   return (
     <aside className='flex flex-col gap-6 border-r border-zinc-200 px-5 py-8'>
       <Logo />
 
-      <div className='flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm'>
-        <Search className='h-5 w-5 text-zinc-500' />
-        <input
-          type='text'
-          className='flex-1 border-0 bg-transparent p-0 text-sm text-zinc-900 placeholder:text-zinc-600 focus:outline-none'
-          placeholder='Search...'
-        />
-      </div>
+      <Input.Root>
+        <Input.Icon>
+          <Search className='h-5 w-5 text-zinc-500' />
+        </Input.Icon>
+        <Input.Control type='text' placeholder='Search...' />
+      </Input.Root>
 
       <nav className='space-y-px'>
         <NavItem title='Home' icon={Home} />
