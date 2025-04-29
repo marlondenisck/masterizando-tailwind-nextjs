48d759be3d5d940f0a0a524249f27d9ee8fddd4c
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2025-04-29 15:36:40 -0400

    composition pattern fileUpload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SettingsTabs } from '@/components/Settings'
 import { Input } from '@/components/Input'
-import { Mail, UploadCloud, User } from 'lucide-react'
+import { Mail } from 'lucide-react'
+import { FileInput } from '@/components/Form/FileInput'
 
 export default function Home() {
   return (
@@ -88,32 +89,11 @@ export default function Home() {
               </span>
             </label>
 
-            <div className='flex items-start gap-5'>
-              <div className='flex h-16 w-16 items-center justify-center rounded-full bg-violet-50'>
-                <User className='h-8 w-8 text-violet-500' />
-              </div>
-
-              <label
-                htmlFor='filePhoto'
-                className='group hover:bg-violet-25 flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:text-violet-500'
-              >
-                <div className='rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100'>
-                  <UploadCloud className='h-5 w-5 text-zinc-600 group-hover:text-violet-600' />
-                </div>
-                <div className='flex flex-col items-center gap-1'>
-                  <span className='text-sm'>
-                    <span className='font-semibold text-violet-700'>
-                      Click to upload
-                    </span>{' '}
-                    or grag and drop
-                  </span>
-                  <span className='text-xs'>
-                    SVG, PNG, JPG or GIF (max. 800x400px)
-                  </span>
-                </div>
-              </label>
-              <input type='file' id='filePhoto' className='sr-only' />
-            </div>
+            <FileInput.Root className='flex items-start gap-5'>
+              <FileInput.ImagePreview />
+              <FileInput.Trigger />
+              <FileInput.Control />
+            </FileInput.Root>
           </div>
 
           {/* ROLE */}
@@ -168,6 +148,10 @@ export default function Home() {
                 Share a few snippets of your work.
               </span>
             </label>
+            <FileInput.Root>
+              <FileInput.Trigger />
+              <FileInput.Control multiple />
+            </FileInput.Root>
           </div>
 
           <div className='flex items-center justify-end gap-2 pt-5'>
---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,12 @@
+import { Control } from './Control'
+import { ImagePreview } from './ImagePreview'
+import { Root } from './Root'
+import { Trigger } from './Trigger'
+
+// export { Control, ImagePreview, Root, Trigger }
+export const FileInput = {
+  Control: Control,
+  ImagePreview: ImagePreview,
+  Root: Root,
+  Trigger: Trigger,
+}
